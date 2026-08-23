14d89866c413c0c88f60c7c57e4142c69614699a
el-j <el-j@users.noreply.github.com>
2026-08-23 12:58:00 +0200

    feat(docker): v0.0.2-alpha.14 release with native debian gh package in agent-workspace

--- build-deb.sh
+++ build-deb.sh
@@ -7,7 +7,7 @@
 set -e
 
 PACKAGE_NAME="openmediavault-agent-station"
-VERSION="0.0.2-alpha.13"
+VERSION="0.0.2-alpha.14"
 ARCH="all"
 DEB_FILE="${PACKAGE_NAME}_${VERSION}_${ARCH}.deb"
 BUILD_DIR="./build-pkg"
