2b9a51a2d00a3a98aab92c89df63839b95cedd0b
el-j <el-j@users.noreply.github.com>
2026-08-23 12:55:42 +0200

    feat(diagnostics): v0.0.2-alpha.12 release with container status alignment and live metrics

--- build-deb.sh
+++ build-deb.sh
@@ -7,7 +7,7 @@
 set -e
 
 PACKAGE_NAME="openmediavault-agent-station"
-VERSION="0.0.2-alpha.11"
+VERSION="0.0.2-alpha.12"
 ARCH="all"
 DEB_FILE="${PACKAGE_NAME}_${VERSION}_${ARCH}.deb"
 BUILD_DIR="./build-pkg"
